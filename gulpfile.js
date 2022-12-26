1609752a145e8a420e80783c22795850eaf70406
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-12-26 10:11:58 -0300

    feat: Sending finished project

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const del = require('del');
-const twig = require('gulp-twig');
-const { argv } = require('yargs');
-const fs = require('fs');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("node-sass"));
+const del = require("del");
+const twig = require("gulp-twig");
+const { argv } = require("yargs");
+const fs = require("fs");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
 
 // Varivel para alterar o tema
-const theme = 'template-01';
+const theme = "template-01";
 
 // Task responsavel por compilar o scss para css
-gulp.task('sass', () => {
+gulp.task("sass", () => {
   return gulp
     .src(`themes/${theme}/assets/css/sass/*.scss`)
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(autoprefixer({ cascade: false }))
     .pipe(gulp.dest(`themes/${theme}/assets/css/`))
-    .pipe(browserSync.stream({ match: '**/*.css' }));
+    .pipe(browserSync.stream({ match: "**/*.css" }));
 });
 
 // Task responsavel por gerar o compilar o twig
-gulp.task('html', async () => {
+gulp.task("html", async () => {
   return gulp
     .src([`themes/${theme}/pages/*.htm`])
     .pipe(
@@ -30,7 +30,7 @@ gulp.task('html', async () => {
         base: `themes/${theme}/`,
         functions: [
           {
-            name: 'source',
+            name: "source",
             func(json) {
               return fs.readFileSync(`themes/${theme}/${json}`);
             }
@@ -38,7 +38,7 @@ gulp.task('html', async () => {
         ],
         filters: [
           {
-            name: 'json_decode',
+            name: "json_decode",
             func(json) {
               return JSON.parse(json);
             }
@@ -46,64 +46,63 @@ gulp.task('html', async () => {
         ]
       })
     )
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest("./dist/"));
 });
 
 // Task que move as imagens de /assets/img para ./dist/src/imagens
-gulp.task('src', () => {
+gulp.task("src", () => {
   return gulp
     .src(`themes/${theme}/assets/img/**.*`)
-    .pipe(gulp.dest('./dist/assets/img/'));
+    .pipe(gulp.dest("./dist/assets/img/"));
 });
 
 const createTmp = argv.create !== undefined;
 
-
 // task do Server...
-gulp.task('serve', () => {
+gulp.task("serve", () => {
   browserSync.init({
-    server: './dist',
+    server: "./dist"
   });
 
   // Monitora as alterações no json
   gulp.watch(
     `themes/${theme}/source/**/*.json`,
-    gulp.series(['clean:css', 'sass', 'clean:html', 'html'])
+    gulp.series(["clean:css", "sass", "clean:html", "html"])
   );
 
   // Monitora as alterações no scss
   gulp.watch(
     `themes/${theme}/assets/css/**/*.scss`,
-    gulp.series(['clean:css', 'sass', 'clean:html', 'html'])
+    gulp.series(["clean:css", "sass", "clean:html", "html"])
   );
 
   // Monitora as alteração nas pastas 'blocks', 'layout', 'pages', 'partials'
-  gulp.watch(`themes/${theme}/**/**.htm`, gulp.series(['clean:html', 'html']));
+  gulp.watch(`themes/${theme}/**/**.htm`, gulp.series(["clean:html", "html"]));
 
   // Faz a sincronização caso haja qualquer modificação no projeto
-  gulp.watch(`themes/${theme}/**/**.*`).on('change', browserSync.reload);
+  gulp.watch(`themes/${theme}/**/**.*`).on("change", browserSync.reload);
 });
 
 // Task de remoção das imagens da pasta '/dist'
 gulp.task(
-  'clean:src',
+  "clean:src",
   gulp.series(() => {
-    return del(['./dist/src/images/**.*']);
+    return del(["./dist/src/images/**.*"]);
   })
 );
 
 // Task de remoção do html
-gulp.task('clean:html', () => {
-  return del(['./dist/*.html']);
+gulp.task("clean:html", () => {
+  return del(["./dist/*.html"]);
 });
 
 // Task de remoção do css
-gulp.task('clean:css', () => {
+gulp.task("clean:css", () => {
   return del([`themes/${theme}/assets/css/*.css`]);
 });
 
 // Task default do gulp, ela é chamada sempre que o comando 'gulp' é executado
 gulp.task(
-  'default',
-  gulp.series(['clean:css', 'clean:html', 'sass', 'html', 'src', 'serve'])
+  "default",
+  gulp.series(["clean:css", "clean:html", "sass", "html", "src", "serve"])
 );
